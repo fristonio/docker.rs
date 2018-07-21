80d8ccec3a934f300be17851c5d5be07b10e6ef8
Deepesh Pathak <deepshpathak@gmail.com>
2018-07-21 20:54:38 +0530

    feat(error-types): add better error types handling to lib (#4)

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -3,6 +3,7 @@ pub mod containers;
 pub mod images;
 pub mod version;
 
+use errors::DockerApiError;
 use utils::Response;
 
 /// Highest level trait for a DockerAPI client
@@ -17,31 +18,31 @@ pub trait DockerApiClient {
     /// It formats the API request using the given parameters, and using
     /// this request the docker daemon and sends back the response of the request
     /// if the request was successful else an err.
+    ///
+    /// This assumes that the request method has been implemented properly
     fn get_response_from_api(
         &self,
         api_endpoint: &str,
         method: &str,
         body: &str,
-    ) -> Result<Response, String> {
+    ) -> Result<Response, DockerApiError> {
         let req = match api_utils::get_formatted_api_request(
             api_endpoint,
             method,
             body,
         ) {
             Some(req) => req,
-            None => return Err("Error while preparing request".to_string()),
+            None => return Err(DockerApiError::RequestPrepareError("Error")),
         };
 
         match self.request(&req) {
-            Some(resp) => match Response::parse_http_response(resp) {
-                Ok(response) => Ok(response),
-                Err(err) => {
-                    Err(format!("Response body was not valid : {}", err))
-                }
-            },
-            None => Err("Got no response from docker host.".to_string()),
+            Some(resp) => Response::parse_http_response(resp),
+            None => Err(DockerApiError::RequestError(
+                "Got no response from docker host.",
+            )),
         }
     }
 
+    /// Implement this function to use this trait.
     fn request(&self, request: &str) -> Option<Vec<u8>>;
 }
--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,5 @@
+use serde_json;
+
 quick_error! {
     #[derive(Debug)]
     pub enum DockerClientError {
@@ -9,6 +11,56 @@ quick_error! {
         SocketConnectionError(addr: &'static str) {
             description("Could not connect to docker socket.")
             display("Error while connection to docker socket at {}", addr)
+        }
+    }
+}
+
+quick_error! {
+    #[derive(Debug)]
+    pub enum DockerApiError {
+        MismatchedParametersError(msg: &'static str) {
+            description("Provided parameters are not valid")
+            display("Malformed parameters : {}", msg)
+        }
+
+        JsonSerializationError(err: serde_json::Error) {
+            description("Error while serializing JSON")
+            display("JSON Serialization error : {}", err)
+        }
+
+        JsonDeserializationError(err: serde_json::Error) {
+            description("Error while deserializing JSON")
+            display("JSON Deserialization error : {}", err)
+        }
+
+        RequestPrepareError(_err: &'static str) {
+            description("An error occured while preparing request")
+            display("Error while preparing request")
+        }
+
+        RequestError(msg: &'static str) {
+            description("Request not handled properly")
+            display("RequestError : {}", msg)
+        }
+
+        HTTPResponseParseError(err: &'static str) {
+            description("Error while parsing response")
+            display("Error while parsing response : {}", err)
+        }
+
+        InvalidApiResponseError(status: usize) {
+            description("Response from Docker API is not valid")
+            display("Invalid API response, status_code : {}", status)
+        }
+
+        ApiRequestError(msg: &'static str) {
+            description("The request to Docker API could not be handled")
+            display("Docker API server Error : {}", msg)
+        }
+
+        ContainerError(msg: String) {
+            description("The container in context faced some error")
+            display("ContainerError : {}", msg)
         }
     }
 }
